94a61c00d472e2ddd2dd70b18ff455efb7c7759a
Saravanan-7 <90743545+Saravanan-7@users.noreply.github.com>
2021-09-28 20:22:11 +0530

    1st file uploaded from pc - yt tut

---
+++ diplay size of data type.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "size of int is " << sizeof(int) << " bytes\n";
+	cout << "Int min Value is " << INT_MIN << endl;
+	cout << "Int max value is " << INT_MAX << endl;
+
+	cout << "size of unsigned int is " << sizeof(unsigned int) << " bytes\n";
+	cout << "UInt max value is " << UINT_MAX << endl;
+	cout << "size of bool is " << sizeof(bool) << " bytes \n";
+	cout << "size of char is " << sizeof(char) << " bytes \n";
+	cout << "size of float is " << sizeof(float) << " bytes \n";
+	cout << "size of double is " << sizeof(double) << " bytes \n";
+	system("pause>0");
+
+}
